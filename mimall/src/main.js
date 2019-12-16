cc6da005d27eab6bfd7dcacd2317965c89d78988
jack <506587246@qq.com>
2019-12-16 23:38:29 +0800

    增加axios的相应拦截器

--- mimall/src/main.js
+++ mimall/src/main.js
@@ -1,8 +1,23 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'
+import VueAxios from 'vue-axios';
 
-Vue.config.productionTip = false
+//拦截器，统一处理错误返回信息
+axios.interceptors.response.use(function (response) {
+  let res = response.data;
+  if (res.status == 0) {
+    return res.data
+  } else if (res.status == 10) {
+    window.location.href = '/#/login'
+  } else {
+    alert(res.msg);
+  }
+});
+
+Vue.Vue(VueAxios, axios);
+Vue.config.productionTip = true
 Vue.config.devtools = true
 
 new Vue({
